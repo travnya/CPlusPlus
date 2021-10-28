036900419bf5f433e0f34ce78e882a97ea5b93b9
travnya <91688913+travnya@users.noreply.github.com>
2021-10-28 10:26:36 +0300

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,157 @@
+﻿#include <iostream>
+using namespace std;
+
+string backgroundColor, foregroundColor, color;
+short choise;
+char symbol;
+
+void menu()
+{
+    choise;
+
+    cout << "===========================================================\n\n";
+    cout << "Привет, я - программа, исполняющая функции, используй меня\n";
+    cout << "Вот, что я могу тебе предоставить: \n\n";
+    cout << "[1] Очистка консоли\n";
+    cout << "[2] Изменение цвета фона консоли\n";
+    cout << "[3] Вывод размера типа данных INT\n";
+    cout << "[4] Вывод размера типа данных CHAR\n";
+    cout << "[5] Вывод размера типа данных DOUBLE\n";
+    cout << "[6] Вывод размера типа данных FLOAT\n";
+    cout << "[7] Вывод размера типа данных BOOL\n";
+    cout << "[8] Вывод сообщения\n";
+    cout << "[9] Вывод квадрата 5х5\n\n";
+    cout << "===========================================================\n\n";
+
+
+    cout << "Выбери что-то одно: ";
+    cin >> choise;
+    cout << "\n\n";
+}
+
+void clearConsole()
+{
+    system("cls");
+}
+
+void changeColor()
+{
+    cout << "Доступные цвета: \n\n";
+    cout << "[0] - Чёрный\n";
+    cout << "[1] - Синий\n";
+    cout << "[2] - Зелёный\n";
+    cout << "[3] - Голубой\n";
+    cout << "[4] - Красный\n";
+    cout << "[5] - Лиловый\n";
+    cout << "[6] - Жёлтый\n";
+    cout << "[7] - Белый\n";
+    cout << "[8] - Серый\n";
+    cout << "[9] - Светло-синий\n";
+    cout << "[A] - Светло-зелёный\n";
+    cout << "[B] - Светло-голубой\n";
+    cout << "[C] - Светло-красный\n";
+    cout << "[D] - Светло-лиловый\n";
+    cout << "[E] - Светло-жёлтый\n";
+    cout << "[F] - Ярко-белый\n\n";
+
+    cout << "Выберите цвет фона: ";
+    cin >> backgroundColor;
+    cout << "\nВыберите цвет текста: ";
+    cin >> foregroundColor;
+
+    color = "color " + backgroundColor + foregroundColor;
+    const char* userColor = color.c_str();
+    system(userColor);
+}
+
+
+void getSizeInt()
+{
+    cout << "Размер типа данных INT = " << sizeof(int) << endl;
+}
+
+void getSizeChar()
+{
+    cout << "Размер типа данных CHAR = " << sizeof(char) << endl;
+}
+
+void getSizeDouble()
+{
+    cout << "Размер типа данных DOUBLE = " << sizeof(double) << endl;
+}
+
+void getSizeFloat()
+{
+    cout << "Размер типа данных FLOAT = " << sizeof(float) << endl;
+}
+
+void getSizeBool()
+{
+    cout << "Размер типа данных BOOL = " << sizeof(bool) << endl;
+}
+
+void getMessage()
+{
+    cout << "Привет, я - функция :3\n";
+}
+
+void printSquare(char)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << symbol << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    setlocale(0, "");
+
+    menu();
+
+    clearConsole();
+
+    switch (choise)
+    {
+    case 1:
+        clearConsole();
+        menu();
+    case 2:
+        changeColor();
+        menu();
+    case 3:
+        getSizeInt();
+        menu();
+    case 4:
+        getSizeChar();
+        menu();
+    case 5:
+        getSizeDouble();
+        menu();
+    case 6:
+        getSizeFloat();
+        menu();
+    case 7:
+        getSizeBool();
+        menu();
+    case 8:
+        getMessage();
+        menu();
+    case 9:
+        cout << "Какой символ вы предпочитаете использовать?";
+        cin >> symbol;
+        printSquare(symbol);
+        menu();
+    default:
+        cout << "Такой функционал пока ещё не реализован\n\n";
+        menu();
+    }
+
+    system("pause");
+    return 01001;
+}
